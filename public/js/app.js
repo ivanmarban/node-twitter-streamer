14afe1ae9f9c4f9db515ea94b404e750446f701d
Ivan Marban <ivan.marban@gmail.com>
2015-11-20 17:13:56 +0100

    webapp files

---
+++ public/js/app.js
@@ -0,0 +1,37 @@
+var socket = io();
+
+$('#track').submit(function(e) {
+	e.preventDefault();
+	var hashtag = $('#hashtag').val().replace('#', '');
+	if (hashtag != '') {
+		socket.emit('track hashtag', hashtag);
+		$('#hashtag').val('');
+		var link = '<a class="close" data-dismiss="alert" id="' + hashtag + '" href="#">&times;</a><p>&num;' + hashtag + '</p>';
+		$('<div class="alert alert-info" id="' + hashtag + '"></div>').html(link)
+			.prependTo('#tracked-hashtags')
+			.css({
+				opacity : 0
+			}).slideDown('slow').animate({
+				opacity : 1
+			}, 'slow');
+	}
+	return false;
+});
+
+socket.on('new tweet', function(tweet) {
+	var div = '<div class="media"><div class="media-left">' +
+		'<img class="media-object img-rounded" src="' + tweet['profileimage'] + '">' +
+		'</div><div class="media-body"><h4 class="media-heading">' + tweet['username'] +
+		'</h4>' + tweet['text'] + '</div></div>';
+	$('<div class="well well-sm"></div>').html(div)
+		.prependTo('#tweets')
+		.css({
+			opacity : 0
+		}).slideDown('slow').animate({
+			opacity : 1
+		}, 'slow');
+});
+
+$('#tracked-hashtags').delegate('a', 'click', function() {
+	socket.emit('untrack hashtag', $(this).attr('id'));
+});
