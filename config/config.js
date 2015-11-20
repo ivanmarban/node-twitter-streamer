abe9713c15e70194b8f69218c2aba8020a5b7ff7
Ivan Marban <ivan.marban@gmail.com>
2015-11-20 17:12:37 +0100

    app config file

---
+++ config/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	twitter : {
+		consumer_key: 			'', //'TWITTER_CONSUMER_KEY',
+		consumer_secret:		'',	//'TWITTER_CONSUMER_SECRET',
+		access_token:			'',	//'TWITTER_ACCESS_TOKEN_KEY',
+		access_token_secret:	'',	//'TWITTER_ACCESS_TOKEN_SECRET'
+	},
+	server : {
+		port: 8080
+	}
+}
